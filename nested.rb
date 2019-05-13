6a6291287479a3ddf598b15c44c09ec00bb9e497
hao wang <ge_nah@hotmail.com>
2019-05-13 05:47:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-  programmer_hash[:alan_key]
+  programmer_hash[:alan_key][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
